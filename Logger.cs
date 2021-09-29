dafab06003589a3e44eaba9f909ef59f858cdd8b
excusemoi <abiturient245@gmail.com>
2021-09-29 20:29:26 +0300

    added

---
+++ Logger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace log
+{
+    public class Logger : IDisposable
+    {
+        
+        public enum _severity
+        {
+            tracer,
+            debug,
+            information,
+            warning,
+            error,
+            critical
+        }
+        private sealed class DisposableEmp : IDisposable
+        {
+            private StreamWriter _streamWriter;
+
+            public DisposableEmp(string filepath)
+            {
+                _streamWriter = new StreamWriter(filepath);
+            }
+
+            public void LoggerWrite(_severity severity, string data)
+            {
+                _streamWriter.Write($"[{DateTime.Now.ToString("f")}]" +
+                                    $"[{severity.ToString("g")}]: {data}\n");
+            }
+            
+            public void Dispose()
+            {
+                _streamWriter.Dispose();    
+            }
+            
+        }
+
+        private DisposableEmp _wr;
+        
+        public Logger(string filepath)
+        {
+            _wr = new DisposableEmp(filepath);
+        }
+
+        public void LoggerWrite(_severity severity, string data)
+        {
+            _wr.LoggerWrite(severity, data);   
+        }        
+        
+        public void Dispose()
+        {
+            _wr.Dispose();
+            GC.SuppressFinalize(this);
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using log;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath, message, sev = "";
+            List<string> messageList = new List<string>();
+            Console.WriteLine("Введите имя файла для ввода сообщений");
+            filepath = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(filepath))
+            {
+                Console.WriteLine("Error: Incorrect path to file");
+                return;
+            }
+            Console.WriteLine("Введите сообщение в формате <severity> <сообщение>\n" +
+                              "severity:\ntracer\ndebug\ninformation\nwarning\nerror\ncritical\n" +
+                              "Пустая строка завершит работу программы");
+            using (var lg = new Logger(Path.Combine(Environment.CurrentDirectory, filepath)))
+            {
+                while (!string.IsNullOrWhiteSpace(message = Console.ReadLine()))
+                {
+                    try
+                    {
+                        Logger._severity severity =
+                            (Logger._severity) Enum.Parse(typeof(Logger._severity),
+                                                                    sev = message.Split(' ')[0], true);
+                        if(Enum.IsDefined(typeof(Logger._severity), severity))
+                            lg.LoggerWrite(severity, message.Substring(sev.Length,
+                                                                        message.Length - sev.Length));
+                        else Console.WriteLine("Error: '{0}' is not a member of Logger._severity enumeration", sev);
+                    }
+                    catch (ArgumentException)
+                    {
+                        Console.WriteLine("Error: '{0}' is not a member of Logger._severity enumeration", sev);
+                    }
+
+                    Console.WriteLine("Введите сообщение в формате <severity> <сообщение>");
+                }
+            }
+        }
+    }
+    
+    
+}
